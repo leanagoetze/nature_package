5e58669080f87799601371ee34dd7767adae83fa
leanagoetze <lrgoetze@ucsb.edu>
2021-03-08 15:50:10 -0800

    adding dataset

---
+++ R/fish_data.R
@@ -0,0 +1,10 @@
+#' fish_data dataset
+#'
+#' Data listing fish species caught
+#' @format A data frame with 300 rows and 1 column
+#' @author Naomi Tague
+#' \describe{
+#' \item{x}{fish species}}
+#' fish_data
+
+##lg not sure about this format, check against Naomi's example. This is just a starting point.
